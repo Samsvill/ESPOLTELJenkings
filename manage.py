ef789431f57b85068d21e50c4f7ac41f7fda539f
Sam Sánchez <88645216+Samsvill@users.noreply.github.com>
2024-08-06 14:11:24 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,25 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import xmlrunner
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'espoltel.settings')
+    
+    # Agregar la configuración de xmlrunner
+    test_runner = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+    test_output_dir = 'test-reports'
+    os.makedirs(test_output_dir, exist_ok=True)
+    
+    # Insertar argumentos de línea de comandos para xmlrunner si no están presentes
+    if 'test' in sys.argv and '--testrunner' not in sys.argv:
+        sys.argv.extend(['--testrunner', test_runner])
+    if 'test' in sys.argv and '--output' not in sys.argv:
+        sys.argv.extend(['--output', test_output_dir])
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
